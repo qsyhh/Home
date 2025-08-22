b05c7e83300441d09073f7af00458ca4fa9e95a2
小雨 <130820022+SHIKEAIXY@users.noreply.github.com>
2025-08-22 20:30:08 +0800

    Update LB.js

--- public/js/LB.js
+++ public/js/LB.js
@@ -1,5 +1,6 @@
 // 赞助者数据
 const sponsors = [
+  { name: "L", amount: 5.00, date: "2025-8-22" },
   { name: "七草", amount: 2.73, date: "2025-8-11" },
   { name: "不嘻嘻", amount: 1.00, date: "2025-8-12" },
   { name: "花生米", amount: 2.00, date: "2025-8-15" },
