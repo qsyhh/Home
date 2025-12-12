613a416403ca994e02440a5e31c24dbef2e335d9
SHIKEAIXY <zsfys225011250@qq.com>
2025-12-12 09:40:35 +0800

    Fix: DH

--- public/js/DH/config.js
+++ public/js/DH/config.js
@@ -5,9 +5,9 @@ const publicConfig = {
         // 备用API：
         backupUrl: "https://picsum.photos/1920/1080.webp",
         // 本地兜底图
-        fallback: "./images/bg-fallback.png"
+        fallback: "../../images/DH/bg-fallback.png"
     },
-    fallbackLogo: "./images/fallback-logo.png",
+    fallbackLogo: "../../images/DH/fallback-logo.png",
     tabMap: {
         hj: "环境", yy: "应用", yx: "游戏", zy: "资源", more: "更多"
     }
--- public/js/DH/nav.js
+++ public/js/DH/nav.js
@@ -116,4 +116,4 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
         `;
     }
-});
+}) 
