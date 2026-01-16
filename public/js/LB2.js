2963ec144a93acecc7e72fcaad45611212f619ec
小雨 <130820022+SHIKEAIXY@users.noreply.github.com>
2026-01-16 20:48:12 +0800

    更新 赞助列表

--- public/js/LB2.js
+++ public/js/LB2.js
@@ -4,4 +4,5 @@ window.sponsorsData = [
   { name: "七草", amount: 2.73, date: "2025-8-11" },
   { name: "不嘻嘻", amount: 1.00, date: "2025-8-12" },
   { name: "花生米", amount: 2.00, date: "2025-8-15" },
+  { name: "Error: Cannot findid", amount: 5.00, date: "2026-1-7" },
 ];
