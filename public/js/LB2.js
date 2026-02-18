41b1afe44ecdda02c6801c7464eb319843029ceb
小雨 <130820022+SHIKEAIXY@users.noreply.github.com>
2026-02-18 22:27:32 +0800

    up 赞助列表

--- public/js/LB2.js
+++ public/js/LB2.js
@@ -2,8 +2,14 @@
 window.sponsorsData = [
   { name: "L", amount: 5.00, date: "2025-8-22" },
   { name: "七草", amount: 2.73, date: "2025-8-11" },
-  { name: "不嘻嘻", amount: 1.00, date: "2025-8-12" },
+  { name: "Fafa", amount: 1.00, date: "2025-8-12" },
   { name: "花生米", amount: 2.00, date: "2025-8-15" },
   { name: "Error: Cannot findid", amount: 5.00, date: "2026-1-7" },
   { name: "霜寂i", amount: 28.88, date: "2026-2-4" },
+   { name: "Fafa", amount: 0.52, date: "2026-2-17" },
+   { name: "弃", amount: 1.20, date: "2026-2-16" },
+   { name: "Fafa", amount: 5.20, date: "2026-2-17" },
+  { name: "二养妮", amount: 1.00, date: "2026-2-18" },
+   { name: "果妹❤️", amount: 8.88, date: "2026-2-18" },
+   { name: "Aozaki Arina", amount: 88.00, date: "2026-2-18" },
 ];
